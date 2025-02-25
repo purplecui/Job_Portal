2aea7b72c99946c3f51f1b9a8dc2e64132962a71
purplecui <gauravkhadka4593@gmail.com>
2025-02-25 21:18:26 +0545

    Revert "auth and usercontroller added"
    This reverts commit c8eb4d76115b024df2fa977c6358e9ae0ca28b3f.

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,29 +1,8 @@
 import express from "express";
-import {
-  createJobController,
-  deleteJobsController,
-  getAllJobsController,
-  jobStatsController,
-  updateJobsController,
-} from "../controller/jobsController.js";
+import { createJobsController } from "../controllers/jobsController.js";
 
 const router = express.Router();
 
-//create JOB
-router.post("/create-jobs", createJobController);
-
-//get JOB
-
-router.get("/get-jobs", userAuth, getAllJobsController);
-
-//update JOB
-
-router.patch("/update-job/:id", userAuth, updateJobsController);
-
-//delete JOB
-router.delete("/delete-job/:id", userAuth, deleteJobsController);
-
-//jobs stack filter
-router.get("/job-stats", userAuth, jobStatsController);
+router.post("/create-jobs", createJobsController);
 
 export default router;
